93250f62cbc14a4f73ff0d9d30b8143f32fd4b74
Gabriel Porras <ghporras@gmail.com>
2021-11-20 18:11:22 -0500

    feat: first build version with StarWars Planets logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+import starWarsLogo from './assets/starwars-planets-header.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={starWarsLogo} className="StarWars Planets" alt="StarWars Planets" />
       </header>
     </div>
   );
