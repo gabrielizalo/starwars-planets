295202f3eceb3506384d6a8c0c45d79b045de32a
Gabriel Porras <ghporras@gmail.com>
2021-11-21 15:57:39 -0500

    Updates

---
+++ precache-manifest.88144e623300355c0c7a3190f4816fd9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111e5416c2d8f874e8c97b8b7aa3b2f9",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "fae176d6e3ee05e0cfe5",
+    "url": "/starwars-planets/static/js/2.20c9409d.chunk.js"
+  },
+  {
+    "revision": "cdffce9574fec3254f704a2dd4332202",
+    "url": "/starwars-planets/static/js/2.20c9409d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42baa903322b3edf82fd",
+    "url": "/starwars-planets/static/js/main.81b95da5.chunk.js"
+  },
+  {
+    "revision": "316c0f13ae2237781c14",
+    "url": "/starwars-planets/static/js/runtime-main.c3acde43.js"
+  },
+  {
+    "revision": "0b703568a23ff6b1be484c2620937af1",
+    "url": "/starwars-planets/static/media/starwars-planets-bg-landscape.0b703568.jpg"
+  },
+  {
+    "revision": "766f0ee1e3c399d72063c646ff69359f",
+    "url": "/starwars-planets/static/media/starwars-planets-bg-portrait.766f0ee1.jpg"
+  },
+  {
+    "revision": "00355ad82235c7e4f2f9e60ec5ae5d13",
+    "url": "/starwars-planets/static/media/starwars-planets-header-landscape.00355ad8.png"
+  },
+  {
+    "revision": "263b818e92092f8338fdaa0fa3b75fd7",
+    "url": "/starwars-planets/static/media/starwars-planets-header-portrait.263b818e.png"
+  }
+]);
