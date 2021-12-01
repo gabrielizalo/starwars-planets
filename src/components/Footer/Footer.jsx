8a58cc290bc5639166933b120750294c9042d7cd
Gabriel Porras <ghporras@gmail.com>
2021-12-01 10:17:08 -0500

    feat: added app and build versioning

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,19 @@ import { Grid, Link, Typography } from '@mui/material';
 import starWarsLogoGitHub from '../../assets/starwars-planet-icon-github.png';
 
 const Footer = function Footer() {
+  const metaAppVersion = document.querySelector('meta[name="app-version"]').content;
+  const appVersion = metaAppVersion !== '%APP_VERSION%' ? `v${metaAppVersion}` : '';
+  const metaBuildVersion = document.querySelector('meta[name="build-version"]').content;
+  const appYear =
+    metaBuildVersion !== '%BUILD_VERSION%' ? metaBuildVersion.substring(0, 4) : '2021';
+
   return (
     <Grid item sx={{ marginTop: 5 }}>
       <footer>
         <Typography variant="body1" align="center">
           Just a simple React exercise by &nbsp;
-          <Link href="https://twitter.com/gabrielizalo">Gabriel Porras</Link> - 2021
+          <Link href="https://twitter.com/gabrielizalo">Gabriel Porras</Link> - {appYear}
+          {appVersion && <span> - {appVersion}</span>}
           <br />
           <Link href="https://github.com/gabrielizalo/starwars-planets">
             <img alt="StarWars Planets GitHub" src={starWarsLogoGitHub} />
